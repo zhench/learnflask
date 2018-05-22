4ddac846aff3c48875bfe7512c2ed88e8aca6e89
shugui <shuguid@hotmail.com>
2018-05-22 23:54:52 +0800

    use db.Model

--- myapp.py
+++ myapp.py
@@ -23,16 +23,36 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 class NameForm(FlaskForm):
     name = StringField('What is you name?', validators=[DataRequired()])
     submit = SubmitField('Submit ')
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'  # define the name of table in database
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %>' % self.username
 
 
 # @app.route('/')
